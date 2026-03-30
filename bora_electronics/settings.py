d6899a4574e3d84be868a1f447e149d52839bf9d
Deno Records Electronics <denorecordselectronics@gmail.com>
2026-03-30 10:41:26 +0300

    fix(db): fall back to project directory for sqlite database on render if DATABASE_URL is unspecified

--- bora_electronics/settings.py
+++ bora_electronics/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,15 +87,11 @@ WSGI_APPLICATION = 'bora_electronics.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
 }
-
-# If we are on Render, use the persistent volume for sqlite
-if 'RENDER' in os.environ:
-    DATABASES['default']['NAME'] = '/var/data/db.sqlite3'
 
 
 # Password validation
